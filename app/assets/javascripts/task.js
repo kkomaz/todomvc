b782e1163e1fa574f849c060a763d9c9ef705e4e
kkomaz <leeajl006@gmail.com>
2015-03-27 08:15:32 -0400

    blur + double click fix

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -1,12 +1,42 @@
 $(function(){
   checkboxListener();
+  editListener();
+  blurListener();
+  submitListener();
 });
-
-function checkboxListener(){
-  $('#todo-list').on('click', '.toggle', toggleCheckbox);
-}
 
 function toggleCheckbox(){
   $(this).parents("li").toggleClass("completed");
   $(this).parents("form").trigger("submit");
 }
+
+function checkboxListener(){
+  $('#todo-list').on('click', '.toggle', toggleCheckbox);
+}
+
+function editListener(){
+  $('#todo-list').on('dblclick',"label", editHandler);
+}
+
+function editHandler(){
+  $(this).parents("li").toggleClass("editing");
+  $(this).parents("li").find(".edit").focus();
+}
+
+function blurListener(){
+  $('#todo-list').on('blur', '.edit_description', blurHandler);
+}
+
+function blurHandler(){
+  $(this).parents("li").toggleClass("editing");
+}
+
+function submitHandler(){
+  $(this).find(".edit").blur();
+  var text = $(this).find(".edit").val();
+  $(this).parents("li").find("label").text(text);
+}
+function submitListener(){
+  $('#todo-list').on('submit', '.edit_description', submitHandler);
+}
+

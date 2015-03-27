c46cbb080c542ea1986408da38b265af91e2c057
kkomaz <leeajl006@gmail.com>
2015-03-26 20:29:53 -0400

    task created + checkbox visualization done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
 class TasksController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.create(task_params)
+
+    respond_to do |f|
+      f.html do 
+        redirect_to @list
+      end
+      f.js {}
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    respond_to do |f|
+      f.html {}
+      f.js {}
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
